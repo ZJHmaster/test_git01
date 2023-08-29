9bb6193d40878260e15d0d555385248aee82e206
ZJHmaster <143492001+ZJHmaster@users.noreply.github.com>
2023-08-29 20:36:58 +0800

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,17 @@
-package org.example;
+package包 org.example;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public公开课Appclass App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("hello dev branch");
-        System.out.println( "Hello fix branch" );
-        System.out.println( "Hello fix branch" );
-        System.out.println( "Hello fix branch" );
-        System.out.println( "Hello fix branch" );
-    }
+    公共静态无效主（字符串[]参数）public static void main( String[] args )
+    {{
+        System.out.println("你好世界！");println( "Hello World!" );
+        System.out.println("你好开发分支");println("hello dev branch");
+        System.out.println( "Hello 修复分支" );println( "Hello fix branch" );
+        System.out.println( "Hello 修复分支" );println( "Hello fix branch" );
+
+    }}
 }
